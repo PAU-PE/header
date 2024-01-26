790db1bb75a5ff2640a4a134cd119486fe2b5ae5
Pau <135823434+PAU-PE@users.noreply.github.com>
2024-01-26 11:01:33 +0100

    allowed_methods.py

--- allowed_methods.py
+++ allowed_methods.py
@@ -1,23 +1,18 @@
-import subprocess
+import requests
 
-url = "http://example.com"
+url = "https://www.tesla.com/"
 methods = ["OPTIONS", "GET", "HEAD", "POST", "DELETE", "TRACE", "PROPFIND", "PROPPATCH", "COPY", "MOVE", "LOCK", "UNLOCK", "PUT"]
 
 for method in methods:
-    modified_command = f"curl -I -X {method} {url}"
-    print(f"Executing: {modified_command}")
-
     try:
-        result = subprocess.run(modified_command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        response_lines = result.stdout.splitlines()
-        http_code = next((line.split()[1] for line in response_lines if line.startswith("HTTP")), None)
+        response = requests.request(method, url, allow_redirects=False)
 
-        if http_code == "200":
-            print(f"Response for {method}: {http_code} OK")
+        if response.status_code == 200:
+            print(f"Response for {method}: {response.status_code} OK")
         else:
-            print(f"Response for {method}: {http_code}")
-        
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing {modified_command}: {e}")
-    
+            print(f"Response for {method}: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error executing {method} request: {e}")
+
     print("-------------------------")
